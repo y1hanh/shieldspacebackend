2045397f12f7bf304998fcd9e63a48cfdb996940
Yihan Huang <yhua0252@student.monash.edu>
2025-05-10 10:26:02 +1000

    add gemini support action plan

--- backend/src/gemini/gemini.controller.ts
+++ backend/src/gemini/gemini.controller.ts
@@ -3,18 +3,18 @@ import { AiEmotionsService, Script } from './gemini.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { Emotions } from 'src/emotions/emotions.controller';
 
-@Controller('script')
+// @UseGuards(AuthGuard)
+@Controller('ai')
 export class AiEmotionsController {
   constructor(private readonly aiService: AiEmotionsService) {}
 
-  // @UseGuards(AuthGuard)
-  @Post('/')
-  async getScript(@Body() userInput: Script): Promise<{}> {
-    return this.aiService.getScript(userInput);
-  }
+  // @Post('/script')
+  // async getScript(@Body() userInput: Script): Promise<{}> {
+  //   return this.aiService.getScript(userInput);
+  // }
 
-  @Post('/education')
-  async getEdu(@Body() userInput: Emotions): Promise<{}> {
-    return this.aiService.getEdu(userInput.user_input);
+  @Post('/action')
+  async getAction(@Body() userInput: Emotions): Promise<{}> {
+    return this.aiService.getAction(userInput.user_input);
   }
 }
--- backend/src/gemini/gemini.service.ts
+++ backend/src/gemini/gemini.service.ts
@@ -1,19 +1,20 @@
-import {
-  Candidate,
-  GenerateContentResponse,
-  GoogleGenAI,
-  Type,
-} from '@google/genai';
+import { Candidate, GoogleGenAI } from '@google/genai';
 import { Injectable } from '@nestjs/common';
-import { response } from 'express';
 
 type scriptResponse = {
   script: string[];
 };
+
+type actionResponse = {
+  'immediate-action': string[];
+  'long-term-skills': string[];
+};
+
 export type Script = {
   user_input: string;
   role: 'parents' | 'teacher' | 'friends';
 };
+
 @Injectable()
 export class AiEmotionsService {
   model = 'gemini-2.0-flash-001';
@@ -63,7 +64,67 @@ export class AiEmotionsService {
     return { script: response };
   }
 
-  async getEdu(userInput: string): Promise<any> {
-    return {};
+  async getAction(userInput: string): Promise<actionResponse> {
+    const config = {
+      responseMimeType: 'text/plain',
+      systemInstruction: [
+        {
+          text: `You are an empathetic and supportive AI assistant, specifically designed to help children handle hurtful online messages and foster emotional resilience. 
+          Your task is to provide an action plan that addresses both the immediate emotional impact of the message and offers long-term strategies to build skills.
+Do not include extra text, explanations. 
+          Structure of the Response do not include asterisk (no more than 200 words):
+1.Immediate Action
+2.Long-Term Skills to Build`,
+        },
+      ],
+    };
+
+    const contents = [
+      {
+        role: 'user',
+        parts: [
+          {
+            text: userInput,
+          },
+        ],
+      },
+    ];
+
+    const { candidates } = await this.ai.models.generateContent({
+      model: this.model,
+      config,
+      contents,
+    });
+
+    let response: actionResponse = {
+      'immediate-action': [],
+      'long-term-skills': [],
+    };
+
+    const { content } = candidates?.[0] as Candidate;
+
+    if (content?.parts?.[0]?.text) {
+      const current = content.parts[0]?.['text']
+        .split('\n')
+        .filter((line) => line.trim() !== '');
+
+      let indexOfSecond = 0;
+      current.forEach((line, index) => {
+        if (line.includes('2.')) {
+          indexOfSecond = index;
+        }
+      });
+      const firstPart = current.slice(1, indexOfSecond);
+      const secondPart = current.slice(indexOfSecond + 1);
+      response = {
+        'immediate-action': firstPart,
+        'long-term-skills': secondPart,
+      };
+    }
+
+    if (!Object.entries(response).length) {
+      throw new Error('No response from AI');
+    }
+    return response;
   }
 }
